43ca859487fc9dcaf37ca20bdf7028a7b99d43da
Thiago Fernandes <thiagofds123@outlook.com>
2023-03-08 18:16:29 -0300

    Fix ListTile

--- lib/app/components/layout/card_store_location/card_store_location.dart
+++ lib/app/components/layout/card_store_location/card_store_location.dart
@@ -5,46 +5,57 @@ class MBRCardStoreLocation extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Card(
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
         child: Column(
           children: [
-            ListTile(
-              title: const Text('Carrefour Market'),
-              subtitle: const Text('Rua Romualdo Lopes Cansado BB, 304 - Bairro Castelo'),
-              leading: Expanded(
-                  child:
-                      Image.network('https://www.infomoney.com.br/wp-content/uploads/2019/06/praia-cacimba-do-padre.jpg?fit=900%2C510&quality=50&strip=all')),
-            ),
             Row(
-              mainAxisAlignment: MainAxisAlignment.end,
               children: [
-                TextButton.icon(
-                  onPressed: () {},
-                  icon: const Icon(Icons.percent),
-                  label: const Text('50% off'),
-                  style: TextButton.styleFrom(
-                    foregroundColor: Colors.blue[900],
-                    backgroundColor: Colors.grey[300],
-                  ),
-                ),
-                TextButton.icon(
-                  onPressed: () {},
-                  icon: const Icon(Icons.star),
-                  label: const Text('4.6'),
-                  style: TextButton.styleFrom(
-                    foregroundColor: Colors.yellow,
-                    backgroundColor: Colors.grey[200],
-                  ),
-                ),
-                TextButton.icon(
-                  onPressed: () {},
-                  icon: const Icon(Icons.location_on),
-                  label: const Text('6,8 KM'),
-                  style: TextButton.styleFrom(
-                    foregroundColor: Colors.black,
-                  ),
+                SizedBox(
+                    width: 100,
+                    child:
+                        Image.network('https://www.infomoney.com.br/wp-content/uploads/2019/06/praia-cacimba-do-padre.jpg?fit=900%2C510&quality=50&strip=all')),
+                Column(
+                  children: [
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.6,
+                      child: const ListTile(
+                        contentPadding: EdgeInsets.zero,
+                        title: Text('Carrefour Market'),
+                        subtitle: Text('Rua Romualdo Lopes Cansado BB, 304 - Bairro Castelo'),
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        TextButton.icon(
+                          onPressed: () {},
+                          icon: const Icon(Icons.percent),
+                          label: const Text('50% off'),
+                          style: TextButton.styleFrom(
+                            foregroundColor: Colors.blue[900],
+                          ),
+                        ),
+                        TextButton.icon(
+                          onPressed: () {},
+                          icon: const Icon(Icons.star),
+                          label: const Text('4.6'),
+                          style: TextButton.styleFrom(
+                            foregroundColor: Colors.yellow,
+                          ),
+                        ),
+                        TextButton.icon(
+                          onPressed: () {},
+                          icon: const Icon(Icons.location_on),
+                          label: const Text('6,8 KM'),
+                          style: TextButton.styleFrom(
+                            foregroundColor: Colors.black,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
                 ),
               ],
             ),
